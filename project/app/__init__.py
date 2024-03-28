adb3c4a53dae6b022d5b8333d27894465b67d7de
Daniel O'Neill <danieloneill18@yahoo.com>
2024-03-28 19:34:19 +0000

    implemented translation for navbar only

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
-
+from flask_babel import Babel
 
 app = Flask(__name__)
 
+def get_locale():
+    return 'ko'
+
+babel = Babel(app, locale_selector=get_locale)
 
 from app import routes
