9715e6cb7409e307699b05637efaed92b6a000ad
Daniel O'Neill <danieloneill18@yahoo.com>
2024-03-29 19:28:38 +0000

    can select locale from navbar, with translation working

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
+app.secret_key = 'a_secret_key'  # Set a secret key for session management
 
 def get_locale():
-    return 'ko'
+    # Retrieve the language from the session if it's set, otherwise default to 'en'
+    return session.get('language', 'en')
 
-babel = Babel(app, locale_selector=get_locale)
+babel = Babel(app)
 
-from app import routes
+babel.init_app(app, locale_selector=get_locale)
+
+@app.route('/change-language/<language>')
+def change_language(language):
+    # Set the selected language in the session
+    session['language'] = language
+    return redirect(url_for('index'))  # Redirect to the homepage after changing the language
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
