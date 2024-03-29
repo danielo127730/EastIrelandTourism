42f51147299a65142ea0429717f5f44194e8cb31
Daniel O'Neill <danieloneill18@yahoo.com>
2024-03-29 23:30:11 +0000

    content switching based on locale

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -1,26 +1,25 @@
-from flask import Flask, session, redirect, url_for, render_template
+from flask import Flask, session
 from flask_babel import Babel
 
-app = Flask(__name__)
-app.secret_key = 'a_secret_key'  # Set a secret key for session management
-
+#This will set the language variable in session for the Babel app
 def get_locale():
-    # Retrieve the language from the session if it's set, otherwise default to 'en'
-    return session.get('language', 'en')
+    if 'language' in session:
+        return session['language']
+    else:
+        session['language']='de'
+    return session['language']
 
+
+#Create the flask app
+app = Flask(__name__)
+#We are going to use the session to manage language then we need to set a secret key
+app.secret_key='a secret key'
+
+#Hook Babel into your app
 babel = Babel(app)
-
+#Initiate the Babel app passing through the locale you want to start with
 babel.init_app(app, locale_selector=get_locale)
 
-@app.route('/change-language/<language>')
-def change_language(language):
-    # Set the selected language in the session
-    session['language'] = language
-    return redirect(url_for('index'))  # Redirect to the homepage after changing the language
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+#Import your routes (you need to configure these in routes.py)
+from app import routes
